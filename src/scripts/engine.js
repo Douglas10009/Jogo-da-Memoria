eeeecad74044638cdb5187bac8f3aea2f1b2cc5a
Douglas <douglasevertonev@gmail.com>
2023-12-23 19:12:43 -0300

    Vitória-Falha/Timer/RandomJunimos/font

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -58,24 +58,109 @@ function checkMatch() {
         openCards[1].classList.remove('boxOpen');
     }
     openCards = [];
-
-
-    // Mostra mensagem de vitória
-    if (document.querySelectorAll('.boxMatch').length === aldeoes.length) {
-        alert('U win!')
-    }
 }
+
+
+
+
+
+// Minhas adições
 
 const playAudio = (file) => {
     const audio = new Audio(`./src/audio/${file}.mp3`);
-    // audio.play();
-    // audio.volume = 0.5;
+    audio.play();
+    audio.volume = 0.5;
 
+}
+
+const state = {
+    visualTimer: document.querySelector('#timer'),
+    semanticTimer: 40,
+    cards: document.querySelectorAll('.item'),
+    imgJunimoHappyCampo: document.querySelector('.img-happy'),
+    imgJunimoSadCampo: document.querySelector('.img-sad'),
+    telaVitoria: document.querySelector('.win'),
+    telaPerda: document.querySelector('.fail'),
+}
+
+console.log(state.semanticTimer);
+
+const setTimer = () => {
+    setInterval(() => {
+        
+        if (state.semanticTimer != 0) {
+            state.semanticTimer--;
+            state.visualTimer.innerHTML = state.semanticTimer;
+        }
+
+        // armengue
+        if (document.querySelectorAll('.boxMatch').length === aldeoes.length || state.semanticTimer == 0) {
+            verifyVictoryFail();
+        }
+
+    }, 1000)
+}
+
+const verifyVictoryFail = () => {
+    let victory = false;
+
+    if (state.semanticTimer == 0) {
+        if (document.querySelectorAll('.boxMatch').length !== aldeoes.length) {
+            victory = false;
+        }
+    }
+
+    if (document.querySelectorAll('.boxMatch').length === aldeoes.length) {
+        victory = true;
+    }
+
+
+    victoryFail(victory);
+}
+
+
+const addRandomJunimos = (victory) => {
+    const srcHappy = ['junimo-purple-happy', 'junimo-green-happy'];
+    const srcSad = ['junimo-red-sad', 'junimo-white-sad'];
+    let randomNumber = Math.round(Math.random() * (srcHappy.length - 1));
+
+
+    let img = document.createElement('img');
+    if (victory) {
+        img.src = `../src/images/${srcHappy[randomNumber]}.gif`;
+    } else {
+        img.src = `../src/images/${srcSad[randomNumber]}.gif`;
+    }
+    img.width = 200
+
+    if (victory) {
+        state.imgJunimoHappyCampo.appendChild(img);
+        state.imgJunimoHappyCampo.classList.add('add'); //Para debuggar
+    } else {
+        state.imgJunimoSadCampo.appendChild(img);
+        state.imgJunimoSadCampo.classList.add('add'); //Para debuggar
+    }
+}
+
+const victoryFail = (victory) => {
+    if (victory) {
+        if (!state.imgJunimoHappyCampo.classList.contains('add')) {
+            addRandomJunimos(victory);
+        }
+        state.telaVitoria.classList.remove('hide');
+    } else {
+        if (!state.imgJunimoSadCampo.classList.contains('add')) {
+            addRandomJunimos(victory);
+        }
+        state.telaPerda.classList.remove('hide');
+
+    }
 }
 
 
 function main() {
-    playAudio('Stardew Valley - Winter (The Wind Can Be Still) - OST.mp3');
+    // playAudio('Stardew Valley - Winter (The Wind Can Be Still) - OST.mp3');
+    setTimer();
 }
 
 main();
